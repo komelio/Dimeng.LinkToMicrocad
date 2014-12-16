c240fa5271f5791936df359a3f916d504009377e
spxie <spxie@dm-software.com>
2014-12-16 11:53:07 +0800

    Add new dimeng helper

---
+++ Dimeng.LinkToMicrocad/Dimeng/DimengHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dimeng.LinkToMicrocad
+{
+    internal class DimengHelper
+    {
+        internal void ShowPromptAndDrawBlock()
+        {
+
+        }
+
+        internal void EditAndDrawBlock()
+        {
+            
+        }
+    }
+}
